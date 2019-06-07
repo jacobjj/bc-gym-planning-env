217aea2f6371ee70ceca6884c342b373124c7215
jacobjj <jjj025@ucsd.edu>
2019-06-06 17:27:15 -0700

    Removed goal normalization from Egocentric class

--- bc_gym_planning_env/envs/egocentric.py
+++ bc_gym_planning_env/envs/egocentric.py
@@ -16,6 +16,7 @@ from bc_gym_planning_env.utilities.costmap_utils import extract_egocentric_costm
 
 class ObservationWrapper(object):
     """ A generic abstract observation wrapper. Override .observation() method in the subclass."""
+
     def __init__(self, env):
         """ Intitialize the observation wrapper.
         :param env object: the environment to wrap.
@@ -110,17 +111,30 @@ class EgocentricCostmap(ObservationWrapper):
         """
         super(EgocentricCostmap, self).__init__(env)
         # As openai gym style requires knowing resolution of the image up front
-        self._egomap_x_bounds = np.array([-0.5, 3.])  # aligned with robot's direction
-        self._egomap_y_bounds = np.array([-2., 2.])  # orthogonal to robot's direction
-        resulting_size = np.array([self._egomap_x_bounds[1] - self._egomap_x_bounds[0],
-                                   self._egomap_y_bounds[1] - self._egomap_y_bounds[0]])
+        self._egomap_x_bounds = np.array([-0.5, 3.
+                                          ])  # aligned with robot's direction
+        self._egomap_y_bounds = np.array([-2., 2.
+                                          ])  # orthogonal to robot's direction
+        resulting_size = np.array([
+            self._egomap_x_bounds[1] - self._egomap_x_bounds[0],
+            self._egomap_y_bounds[1] - self._egomap_y_bounds[0]
+        ])
 
-        pixel_size = world_to_pixel(resulting_size, np.zeros((2,)), resolution=0.03)
+        pixel_size = world_to_pixel(resulting_size,
+                                    np.zeros((2, )),
+                                    resolution=0.03)
         data_shape = (pixel_size[1], pixel_size[0], 1)
-        self.observation_space = spaces.Dict(OrderedDict((
-            ('env', spaces.Box(low=0, high=255, shape=data_shape, dtype=np.uint8)),
-            ('goal', spaces.Box(low=-1., high=1., shape=(3, 1), dtype=np.float64))
-        )))
+        self.observation_space = spaces.Dict(
+            OrderedDict((('env',
+                          spaces.Box(low=0,
+                                     high=255,
+                                     shape=data_shape,
+                                     dtype=np.uint8)),
+                         ('goal',
+                          spaces.Box(low=-1.,
+                                     high=1.,
+                                     shape=(3, 1),
+                                     dtype=np.float64)))))
 
     def observation(self, observation):
         """Extract egocentric map and path from rich observation
@@ -133,28 +147,36 @@ class EgocentricCostmap(ObservationWrapper):
         ego_costmap = extract_egocentric_costmap(
             costmap,
             robot_pose,
-            resulting_origin=(self._egomap_x_bounds[0], self._egomap_y_bounds[0]),
-            resulting_size=np.array([self._egomap_x_bounds[1] - self._egomap_x_bounds[0],
-                                     self._egomap_y_bounds[1] - self._egomap_y_bounds[0]])
-        )
+            resulting_origin=(self._egomap_x_bounds[0],
+                              self._egomap_y_bounds[0]),
+            resulting_size=np.array([
+                self._egomap_x_bounds[1] - self._egomap_x_bounds[0],
+                self._egomap_y_bounds[1] - self._egomap_y_bounds[0]
+            ]))
 
         ego_path = from_global_to_egocentric(observation.path, robot_pose)
         obs = np.expand_dims(ego_costmap.get_data(), -1)
         if len(ego_path) == 0:
             # 3 is goal len + 6 is len of the state
-            size_of_goal_n_state = 3 + observation.robot_state.to_numpy_array().shape[0]
+            size_of_goal_n_state = 3 + observation.robot_state.to_numpy_array(
+            ).shape[0]
             return OrderedDict((
                 ('env', obs),
                 # 9 values characterize robot's state including velocities
-                ('goal_n_state', np.expand_dims(np.zeros(size_of_goal_n_state, dtype=np.float32), -1))
-            ))
+                ('goal_n_state',
+                 np.expand_dims(
+                     np.zeros(size_of_goal_n_state, dtype=np.float32), -1))))
         else:
-            normalized_goal = ego_path[0, :2] / ego_costmap.world_size()
-            normalized_goal = np.clip(normalized_goal, (-1., -1.), (1., 1.))
-            goal_pose = np.hstack([normalized_goal, ego_path[0, 2]])
-            goal_n_state = np.hstack([goal_pose, observation.robot_state.to_numpy_array()])
+            # normalized_goal = ego_path[0, :2] / ego_costmap.world_size()
+            # normalized_goal = np.clip(normalized_goal, (-1., -1.), (1., 1.))
+            # Just pass the actual goal, rather than normalizing it!!
+            goal = ego_path[0, :2]
+            goal_pose = np.hstack([goal, ego_path[0, 2]])
+            goal_n_state = np.hstack(
+                [goal_pose,
+                 observation.robot_state.to_numpy_array()])
             # observation.robot_state is: wheel_angle, measured_v, measured_w, steering_motor_command
-            return OrderedDict((
-                ('env', obs),
-                ('goal_n_state', np.expand_dims(goal_n_state, -1).astype(np.float32))
-             ))
+            return OrderedDict(
+                (('env', obs), ('goal_n_state',
+                                np.expand_dims(goal_n_state,
+                                               -1).astype(np.float32))))
